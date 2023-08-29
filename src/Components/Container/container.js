3968fe7fe543c59113ffc92d3ac1b0bec8f78b57
krambecktiago <tiago@krambeck.com.br>
2023-08-29 17:05:17 -0300

    Criando estilização da página de login

--- src/Components/Container/container.js
+++ src/Components/Container/container.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 import './container.css'
+import Login from '../../Pages/Login/Login'
 
 const Container = () => (
     <main className="app-container">
+        <Login/>
     </main>
 )
 
--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+//estilização
+import './login.css'
+
 const Login = () => {
     return(
-        <>
-            <h1>Isso aqui vai ficar foda</h1>
-        </>
+        <main>
+            <form className="form-content">
+                <div className="title-form">
+                    <h1>BEM VINDO!</h1>
+                    <h5>Faça o login para continuar</h5>
+                </div>
+                <div className="input-form-login">
+                    <span>LOGIN: </span>
+                    <input type="text" placeholder="Insira seu login"/>
+                </div>
+                <div className="input-form-senha">
+                    <span>SENHA: </span>
+                    <input type="password" placeholder="Insira sua senha"/>
+                </div> 
+            </form>
+        </main>
     )
 }
 
